7d1f6372996c2d6d1f51a54d317984f94eab226e
Badr Choubai <badr.choubaidev@outlook.com>
2019-08-28 13:45:51 -0600

    Numbers.java

---
+++ Minis/Numbers.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> userInput = new ArrayList<Integer>();
+        
+        do {
+            System.out.println("Give me a number");
+            int num = scanner.nextInt();
+            userInput.add(num);
+        } while (userInput.size() < 2);
+        
+        int sumFromarray = getSumFromArray(userInput);
+        int productFromArray = getProductFromArray(userInput);
+        double quotientFromArray = getQuotientFromArray(userInput);
+        System.out.printf("Sum : %s \n",sumFromarray);
+        System.out.printf("Product : %s \n",productFromArray);
+        System.out.printf("Quotient : %s \n", quotientFromArray);
+
+        scanner.close();
+    };
+
+    public static int getSumFromArray(ArrayList<Integer> input) {
+        int sum = 0;
+
+        for (int i = 0; i < input.size(); i++) {
+            sum+=input.get(i);
+        }
+
+        return sum;
+    };
+
+    public static int getProductFromArray(ArrayList<Integer> input) {
+        int product = 1;
+
+        for (int i = 0; i < input.size(); i++) {
+            product*=input.get(i);
+        }
+
+        return product;
+    };
+
+    public static double getQuotientFromArray(ArrayList<Integer> input) {
+        double toDouble = (double) input.indexOf(0);
+
+        return toDouble / input.indexOf(1);
+    };
+}
