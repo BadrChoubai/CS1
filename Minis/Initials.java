9d135477bbc38b5bd4e831aa914954082ebf04a5
Badr Choubai <badr.choubaidev@outlook.com>
2019-08-28 09:37:22 -0600

    add Initials.java

---
+++ Minis/Initials.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Initials {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] nameParts = {"First", "Middle", "Last"};
+        String initials = "";
+
+        for (int i = 0; i < nameParts.length; i++) {
+            System.out.printf("What is your %s name? \n", nameParts[i]);
+            String answer = scanner.nextLine();
+            initials += answer.charAt(0);
+        };
+        
+        scanner.close();
+        System.out.println(initials.replace(" ", "."));
+    };
+}
