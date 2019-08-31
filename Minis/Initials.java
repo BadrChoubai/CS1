3907ec9223efedd7b687e1f850e67359eefade1c
Badr Choubai <badr.choubaidev@outlook.com>
2019-08-30 21:11:12 -0600

    Initials final solution

--- Minis/Initials.java
+++ Minis/Initials.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 class Initials {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char firstInitial, middleInitial, lastInitial;
-        String initials;
+        String initials = "";
+        String[] fullName = { "first", "middle", "last" };
 
-        System.out.println("What is your first name");
-        firstInitial = scanner.nextLine().charAt(0);
-        System.out.println("What is your middle name");
-        middleInitial = scanner.nextLine().charAt(0);
-        System.out.println("What is your last name");
-        lastInitial = scanner.nextLine().charAt(0);
-
-        initials = String.format("%s.%s.%s.", firstInitial, middleInitial, lastInitial);
-
+        for (String namePart : fullName) {
+            System.out.printf("What is your %s name? \n", namePart);
+            String answer = scanner.nextLine();
+            if (answer.length() > 1) {
+                initials += answer.substring(0, 1);
+            }
+        }
         scanner.close();
-        System.out.println(initials);
+
+        System.out.println(initials.toUpperCase());
     };
 }
