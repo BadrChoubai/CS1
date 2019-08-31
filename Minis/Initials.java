8a731ab116fda18c612372c768c392181a34449a
Badr Choubai <badr.choubaidev@outlook.com>
2019-08-30 21:45:04 -0600

    small bug fix

--- Minis/Initials.java
+++ Minis/Initials.java
@@ -22,7 +22,7 @@ class Initials {
         for (String namePart : fullName) {
             System.out.printf("What is your %s name? \n", namePart);
             String answer = scanner.nextLine();
-            if (answer.length() > 1) {
+            if (answer.length() >= 1) {
                 initials += answer.substring(0, 1);
             }
         }
