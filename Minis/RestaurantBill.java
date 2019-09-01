89eb7bc8e8a9d7b7e98bb1e0363ae2467ea0ab06
Badr Choubai <badr.choubaidev@outlook.com>
2019-09-01 15:58:26 -0600

    create variable for tax percentage and change naming

--- Minis/RestaurantBill.java
+++ Minis/RestaurantBill.java
@@ -17,8 +17,9 @@ class RestaurantBill {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double billTotal;
-        int tipPercentage;
-        double[] percentageAmounts = { 0, 0.10, 0.15, 0.20 };
+        int tipPercentageSelection;
+        double taxPercentage = 0.7;
+        double[] tipPercentageAmounts = { 0, 0.10, 0.15, 0.20 };
 
         System.out.println("How much was your bill");
         billTotal = scanner.nextDouble();
@@ -28,17 +29,17 @@ class RestaurantBill {
         System.out.println("(1) 10% Tip");
         System.out.println("(2) 15% Tip");
         System.out.println("(3) 20% Tip");
-        tipPercentage = scanner.nextInt();
+        tipPercentageSelection = scanner.nextInt();
 
         scanner.close();
-        double billTotalWithTip = calculateTotalWithTip(billTotal, percentageAmounts[tipPercentage], 0.7);
+        double billTotalWithTip = calculateTotalWithTip(billTotal, tipPercentageAmounts[tipPercentageSelection], taxPercentage);
         System.out.println(billTotalWithTip);
     }
 
-    public static double calculateTotalWithTip(double billTotal, double tipPercentage, double tax) {
+    public static double calculateTotalWithTip(double billTotal, double tipPercentage, double taxPercentage) {
         double result;
 
-        result = billTotal + (billTotal * tipPercentage) + (billTotal * tax);
+        result = billTotal + (billTotal * tipPercentage) + (billTotal * taxPercentage);
 
         return result;
     }
