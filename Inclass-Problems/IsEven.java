d49d8442f7b05f74bc6d654f274c8c93db34b094
Badr Choubai <badr.choubaidev@outlook.com>
2019-09-03 21:03:30 -0600

    rewrite after code review

--- Inclass-Problems/IsEven.java
+++ Inclass-Problems/IsEven.java
@@ -1,14 +1,10 @@
 class IsEven {
     public static void main(String[] args) {
         int input = 2;
-        if (isEven(input)) {
+        if (input % 2 == 0) {
             System.out.printf("%s is an even number \n", input);
         } else {
             System.out.printf("%s is an odd number \n", input);
         }
-    }
-    
-    public static boolean isEven(int input) {
-        return input % 2 == 0;
-    }
+    }    
 }
