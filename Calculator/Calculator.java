0d08bd40e002d5f723d7a774a8a2515e28b4cdf1
Badr Choubai <badr.choubaidev@outlook.com>
2019-09-13 09:27:36 -0600

    start from scratch -> Calculator.java

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,57 +1,30 @@
 import java.util.Scanner;
 
 class Calculator {
-    private Scanner prompter;
-    private int solveCounter;
-    private int additionCount;
-    private int subtractionCount;
-    private int multiplicationCount;
-    private int divisionCount;
+    private Scanner scanner;
+    private int totalSolvedCount, additionSolveCounter, subtractionSolveCounter, multiplicationSolveCounter, divisionSolveCounter;
 
     Calculator() {
-        this.prompter = new Scanner(System.in);
-        this.solveCounter = 0;
+        this.scanner = new Scanner(System.in);
+        this.totalSolvedCount = 0;
+        this.additionSolveCounter = 0;
+        this.subtractionSolveCounter = 0;
+        this.multiplicationSolveCounter = 0;
+        this.divisionSolveCounter = 0;
     }
-
-    public void printIntro() {
-        System.out.println("Welcome to Calculator v0.1 \n");
-    }
-
-    public void calculate() {
-        String[] operationChoices = {"Addition", "Subtraction", "Multiplication", "Division"};
-        int userOperationChoice;
-        Character operation;
     
-        System.out.println("Choose a mathematical operation");
-        for (int i = 0; i < operationChoices.length; i++) {
-            System.out.printf("(%s) - %s \n", i+1, operationChoices[i]);
-        }
-
-        userOperationChoice = this.prompter.nextInt() - 1;
-
-        switch(operationChoices[userOperationChoice].toLowerCase()) {
-            case "addition":
-                operation = '+';
-                break;       
-            case "subtraction":
-                operation = '-';
-                break;       
-            case "multiplication":
-                operation = '*';
-                break;       
-            case "division":
-                operation = '/';
-                break;     
-            default:
-                operation = '_';
-        }
-
-
+    private void printIntro() {
+        String systemUser = System.getProperty("user.name");
+        System.out.printf("Hello %s welcome to Calculator v.0.0.1 \n\n", systemUser);
     }
+    private void calculate() {}
+    private void printResult() {};
 
     public static void main(String[] args) {
-        Calculator main = new Calculator();
-        main.printIntro();
-        main.calculate();
+        Calculator calculator = new Calculator();
+        
+        calculator.printIntro();
+        calculator.calculate();
+        calculator.printResult();
     }
 }
