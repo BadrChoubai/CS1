40e1dac8afd68d0e3bdc69544ac4fd07eb6bf6b5
Badr Choubai <badrchoubai@outlook.com>
2020-02-10 19:11:09 -0700

    (Minis): refactor initials.py

--- Minis/initials.py
+++ Minis/initials.py
@@ -1,39 +1,32 @@
 """
 
-	Program: Initials;
-	Author: Badr Choubai;
-	Date: 2019-08-30;
-    Description: A program that takes a user's full first, middle (if they have one) and, last name. It will return the user's initials;
-
-    Input: User's full name;
-    Output: User's initials;
+    Program: Initials 
+    Author: Badr Choubai 
+    Description: A program that takes a user's full first, middle (if they have one) and, last name. It will return the user's initials.
+    Input: User's full name 
+    Output: User's initials 
 
 """
 
+def get_user_input(prompts: list) -> list:
+    prompt_answers: list = [] 
 
-def get_name_input(name_part: str) -> str:
-	name_part = input(str(f"What is your {name_part} name?: "))
-	return name_part
+    for prompt_message in prompts:
+        answer = input(f"{ prompt_message }: ")
+        prompt_answers.append(answer.strip())
 
-
-def make_initials(full_name: list) -> str:
-	for i, name_part in enumerate(full_name):
-		name_part = name_part.strip()
-		if (name_part):
-			full_name[i] = name_part[:1] + '.'
-
-	return full_name
+    return prompt_answers
 
 
 def main():
-	first_name = get_name_input("first")
-	middle_name = get_name_input("middle")
-	last_name = get_name_input("last")
+    prompts: list = ["What is your first name?", "What is your middle name?", "What is your last name?"]
 
-	full_name = [first_name, middle_name, last_name]
-	initials = ''.join(make_initials(full_name))
-	print(initials)
-
+    [first_name, middle_name, last_name] = get_user_input(prompts)
+   
+    print(f"{ last_name.title() }, { first_name.title() } { middle_name or  '' }")
+    print(f"{ first_name[0].upper() }{ middle_name[0].upper() if middle_name else '' }{ last_name[0].upper() }") 
+    
+    
 
 if __name__ == "__main__":
 	main()
