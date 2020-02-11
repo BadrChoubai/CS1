20d39190e0f655918f90941a24d13e1938082a70
Badr Choubai <badrchoubai@outlook.com>
2020-02-10 19:10:50 -0700

    (Hello CS1): refactor

--- HelloCS1/hello_CS1.py
+++ HelloCS1/hello_CS1.py
@@ -2,6 +2,14 @@ def hello_CS1():
 	print("Hello World!")
 
 	name = 'Badr Choubai'
+        
 	print(f"Hello {name}!")
+	print("Goodbye Cruel World")
 
-	print("Goodbye World")
+
+def main():
+    hello_CS1()
+    
+
+if __name__ == "__main__":
+    main()
