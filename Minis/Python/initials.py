82ebb1dbce3fe0b1f625192121f1e695da411f0a
Badr Choubai <badrchoubai@outlook.com>
2020-02-26 22:38:31 -0700

    (Minis): Refactor initials solution

--- Minis/Python/initials.py
+++ Minis/Python/initials.py
@@ -1,32 +1,16 @@
 """
-
     Program: Initials 
-    Description: A program that takes a user's full first, middle (if they have one) and, last name. It will return the user's initials.
-    Input: User's full name 
-    Output: User's initials 
-
+    Description: A program that asks a user for their first, middle? and, last name. Print the user's initials 
 """
 
 
-def get_user_input(prompts: list) -> list:
-    prompt_answers: list = []
-
-    for prompt_message in prompts:
-        answer = input(f"{ prompt_message }: ")
-        prompt_answers.append(answer.strip())
-
-    return prompt_answers
-
-
 def main():
-    prompts: list = ["What is your first name?",
-                     "What is your middle name?", "What is your last name?"]
+    first_name = input("Give me your first name: ")
+    middle_name = input("Give me your middle name: ") or ''
+    last_name = input("Give me your last name: ")
 
-    [first_name, middle_name, last_name] = get_user_input(prompts)
-
-    print(f"{ last_name.title() }, { first_name.title() } { middle_name or  '' }")
-    print(
-        f"{ first_name[0].upper() }{ middle_name[0].upper() if middle_name else '' }{ last_name[0].upper() }")
+    initials = f'{ first_name[0] }.{ ( middle_name[0] + "." ) if middle_name else "" }{ last_name[0] }.'
+    print(initials.upper())
 
 
 if __name__ == "__main__":
