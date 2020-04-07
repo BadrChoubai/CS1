188cb2178671a7e838118d14592b5386e5fbb1d9
Badr Choubai <badrchoubai@outlook.com>
2020-04-07 07:47:51 -0600

    (inclass problems): compare_first_and_last.py

--- Inclass-Problems/Python/compare_first_and_last.py
+++ Inclass-Problems/Python/compare_first_and_last.py
@@ -1,8 +1,6 @@
 def compare_first_and_last(word: str) -> str:
-    first_character, last_character = word[0], word[-1]
-
     return "first and last characters are the same" if \
-        first_character == last_character else \
+        word[0] == word[-1] else \
         "first and last characters are not the same"
 
 
