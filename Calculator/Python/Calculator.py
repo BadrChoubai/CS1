c89b67e58968ddd6ca9ad8649d0596117c6c1b31
Badr Choubai <badrchoubai@outlook.com>
2020-05-09 23:20:22 -0600

    (Calculator): Finish Python implementation of Calculator program

--- Calculator/Python/Calculator.py
+++ Calculator/Python/Calculator.py
@@ -2,30 +2,48 @@ import sys
 
 
 class Calculator():
-    total_solved_count = 0
-    addition_solve_counter = 0
-    subtraction_solve_counter = 0
-    multiplication_solve_counter = 0
-    division_solve_counter = 0
-    modulo_solver_counter = 0
+    counters = {
+        'addition': 0,
+        'subtraction': 0,
+        'multiplication': 0,
+        'division': 0,
+        'modulo': 0,
+    }
 
-    @classmethod
-    def print_intro(cls):
+    @staticmethod
+    def print_intro():
         print(f'''
         Welcome to CalcPy a Python Calculator program.
         This program will enable users to solve basic maths problems.
         ''')
 
-    @classmethod
-    def print_results(cls):
-        pass
+    def print_results(self):
+        print('Calculator Program Stats:\n')
+        total_solved_count = sum(self.counters.values())
+        for operation in self.counters.keys():
+            print(
+                f'{operation.title()} problems solved: {self.counters[operation]}')
 
-    @classmethod
-    def run(cls):
+        print('Total problems solved: ', total_solved_count)
+
+    @staticmethod
+    def solve(operation: str, x: int, y: int) -> int:
+        return {
+            'ADDITION': (lambda x, y: x + y),
+            'SUBTRACTION': (lambda x, y: x - y),
+            'MULTIPLICATION': (lambda x, y: x * y),
+            'DIVISION': (lambda x, y: x / y),
+            'MODULO': (lambda x, y: x % y),
+        }[operation.upper()](x, y)
+
+    def run(self):
         operation_choice = ""
         operation_choices = ["addition", "subtraction",
                              "multiplication", "division", "modulo"]
-        while True:
+        operand_one, operand_two = None, None
+
+        using = True
+        while using:
             print("What operation would you like to perform? ")
             for i, choice in enumerate(operation_choices):
                 print(f'({i+1}) {choice}')
@@ -34,15 +52,25 @@ class Calculator():
             operation_choice = input('operation choice: ')
 
             if operation_choice == '0':
-                sys.exit()
+                using = False
             elif int(operation_choice) > 5:
                 print('Please double check your selection.')
             else:
                 operation_choice = operation_choices[int(operation_choice) - 1]
-                print(operation_choice)
+                print(f'You chose: {operation_choice}')
+
+                operand_one = int(input('Give me a number: '))
+                operand_two = int(input('Give me another number: '))
+
+                result = self.solve(
+                    operation_choice, operand_one, operand_two)
+                self.counters[operation_choice] += 1
+                print(
+                    f'The {operation_choice} result of {operand_one} and {operand_two} is {result}\n')
 
 
 calculator = Calculator()
+
 calculator.print_intro()
 calculator.run()
 calculator.print_results()
