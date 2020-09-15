d1ec0be7a83fae55f3b969feda35ad37407a478a
Badr Choubai <badrchoubai@outlook.com>
2020-09-15 12:11:52 -0600

    Initial scaffolding of GuessingGame code project

---
+++ Miscellaneous/GuessingGame.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class GuessingGame {
+    /**
+     * @class GuessingGame
+     *
+     * ```
+     * usage:
+     *
+     * GuessingGame gameInstanceName = new GuessingGame();
+     * gameInstanceName.play() // Starts the game;
+     * ```
+     */
+
+    private static Scanner inputScanner = new Scanner(System.in);
+    public static boolean playing = true;
+    public static int number;
+
+    public int getGuess()
+    {
+        // TODO : Implement logic for fetching, validating and returning user input;
+    }
+
+    public void play()
+    {
+        // TODO : implement game logic
+    }
+
+
+    public GuessingGame() {
+        this.playing = true;
+        this.number = (int) (1 * Math.random()) * 100;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to JGuess v:0.0.1");
+
+        GuessingGame gameInstance = new GuessingGame();
+
+        gameInstance.play();
+    }
+}
