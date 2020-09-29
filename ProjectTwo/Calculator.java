1d4381a4d3c3022586e82a90c36f2f10406b8b0c
Badr Choubai <badrchoubai@outlook.com>
2020-09-28 20:03:54 -0600

    Start project 2

---
+++ ProjectTwo/Calculator.java
@@ -0,0 +1,27 @@
+/*
+ * Program Name: Calculator
+ * Description: A small calculator program giving users the ability to perform simple math operations
+ * from their computer
+ * Author: Badr Choubai
+ * Date: September 28, 2020
+ * Version: 01
+ * Vocabulary Word:
+ *
+ *     System ~ n. A set of elements or parts that is coherently organized and interconnected in a pattern
+ *                 or structure that produces a characteristic set of behaviors, often classified as its
+ *                 function or purpose.
+ *
+ * Inspirational Quote:
+ *
+ *     “Antifragility is beyond resilience or robustness.
+ *      The resilient resists shocks and stays the same;
+ *      the antifragile gets better.”
+ *      ~ Nassim Nicholas Taleb, Antifragile
+ *
+ */
+class Calculator {
+    public static void main(String[] args)
+    {
+        System.out.println("JCalc v.0.0.1");
+    }
+}
