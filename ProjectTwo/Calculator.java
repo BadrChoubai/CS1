299005ca4ce89e6c6b8eb92009c92078728bd61e
Badr Choubai <badrchoubai@outlook.com>
2020-09-28 20:47:15 -0600

    feature: printIntro

--- ProjectTwo/Calculator.java
+++ ProjectTwo/Calculator.java
@@ -4,7 +4,7 @@
  * from their computer
  * Author: Badr Choubai
  * Date: September 28, 2020
- * Version: 01
+ * Version: 02
  * Vocabulary Word:
  *
  *     System ~ n. A set of elements or parts that is coherently organized and interconnected in a pattern
@@ -23,9 +23,24 @@ import java.util.Scanner;
 
 class Calculator {
     private Scanner inputScanner = new Scanner(System.in);
+    int additionCount, subtractionCount, divisionCount, multiplicationCount = 0;
+    int totalProblemsSolved = 0;
+    Calculator() {}
+
+    private static void printIntro()
+    {
+        System.out.println("Welcome to JCalc v.0.2");
+        System.out.println("This program helps you solve simple math problems.\n");
+        System.out.println("First, we'll prompt you for the type of operation you'd like to perform");
+        System.out.println("\n\tAddition, Subtraction, Multiplication or Division\n");
+        System.out.println("Then, we'll prompt you for two different numbers (Integers only)");
+        System.out.print("\nYou can solve as many problems as you want, and we'll keep track of it. Happy Solving!");
+
+    }
+
 
     public static void main(String[] args)
     {
-        System.out.println("JCalc v.0.0.1");
+        printIntro();
     }
 }
