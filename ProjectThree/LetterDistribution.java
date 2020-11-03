5e0c212ff8ea613fae1d443a68e77e350fc556b9
Badr Choubai <badrchoubai@outlook.com>
2020-11-03 10:08:38 -0700

    Calculate total letter count

--- ProjectThree/LetterDistribution.java
+++ ProjectThree/LetterDistribution.java
@@ -33,7 +33,8 @@ public class LetterDistribution
         {
             for (char c : fileScanner.next().toCharArray())
             {
-                if (letterFrequency.keySet().contains(c)) {
+                if (letterFrequency.keySet().contains(c))
+                {
                     int curr = letterFrequency.get(c);
                     curr++;
                     letterFrequency.put(c, curr);
@@ -43,10 +44,13 @@ public class LetterDistribution
 
         fileScanner.close();
 
-        for (char c : letterFrequency.keySet())
-        {
-            System.out.println(c);
-            System.out.println(letterFrequency.get(c));
-        }
+        // Get the total letter count
+        // https://docs.oracle.com/javase/tutorial/collections/streams/reduction.html
+        int totalLetterCount = letterFrequency
+            .values()
+            .stream()
+            .reduce(0, (a, b) -> a + b);
+
+        System.out.println(totalLetterCount);
     }
 }
