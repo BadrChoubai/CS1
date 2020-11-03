66a7c654e69d9fe0e1d60bd6a6dca62501c3d429
Badr Choubai <badrchoubai@outlook.com>
2020-11-03 11:46:56 -0700

    finish

--- ProjectThree/src/LetterDistribution.java
+++ ProjectThree/src/LetterDistribution.java
@@ -9,14 +9,14 @@ public class LetterDistribution
 {
     static final String LETTERS_IN_THE_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     static final int    FIRST_LETTER            = 'a';
-    static final String FILE_NAME               = "Constitution.txt";
+    static final String FILE_NAME               = "src/Constitution.txt";
     static final int    FRAME_WIDTH             = 1024;
     static final int    FRAME_HEIGHT            = 768;
 
     public static void main(String[] args) throws FileNotFoundException
     {
         /* Utilizing a map allows us to keep track of all our application's
-         * We need to be able to track each letter's occurence
+         * We need to be able to track each letter's occurrence
          * to display in the barchart
          */
         Map<Character, Integer> letterFrequency = new HashMap<Character, Integer>(26);
@@ -52,14 +52,29 @@ public class LetterDistribution
             .stream()
             .reduce(0, (a, b) -> a + b);
 
-        // convert each letter count to it's percentage of the total letter count
+        // convert each letter count to its percentage of the total letter count
         letterFrequency
             .values()
             .stream()
             .forEach(count -> {
                 double percentage = Math.floor(((double) count / (double) totalLetterCount) * 100);
                 percentages.add(percentage);
-                System.out.println(percentage);
             });
+
+        double[] finalPercentages = percentages.stream().mapToDouble(i -> i).toArray();
+
+        JFrame f = new JFrame();
+        f.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        String title = FILE_NAME.split("\\.")[0];
+        BarChart barchart = new BarChart(title, LETTERS_IN_THE_ALPHABET.split(""), finalPercentages);
+        f.getContentPane().add(barchart);
+
+        WindowListener wndCloser = new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        };
+        f.addWindowListener(wndCloser);
+        f.setVisible(true);
     }
 }
